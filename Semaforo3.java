c584972a85e7ab85e7b09291f0fdab86a2c893fd
LUIS IGNACIO BAHAMONDES PASTÉN <luis.bahamondes.14@sansano.usm.cl>
2019-03-31 23:23:05 -0300

    Stage1 cambios funcionales no optimizado

--- Semaforo3.java
+++ Semaforo3.java
@@ -14,37 +14,7 @@ public class Semaforo3{
     public Semaforo3(){}
 
     public Semaforo3 (int greenTime) {
-        LocalTime ti = LocalTime.now();
-        LocalTime tf = LocalTime.now(); 
-        //verde
-        while(tf.getSecond()-ti.getSecond()<greenTime){
-            int dif0=tf.getSecond()-ti.getSecond();
-            turnGreenLightON();
-            //System.out.println( dif0 + "[s] 0");
-            tf=LocalTime.now();
-        }
-        //amarillo
-        ti = LocalTime.now();
-        tf = LocalTime.now(); 
-
-        while(tf.getSecond()-ti.getSecond()<yellowTime){
-            int dif1=tf.getSecond()-ti.getSecond();
-            turnYellowLightON();
-            //System.out.println(dif1 + " [s]  1");
-            tf=LocalTime.now();
-        }
-        //Rojo
-        tf = LocalTime.now(); 
-        ti = LocalTime.now();
-
-        System.out.println(redTime);
-        while(tf.getSecond()-ti.getSecond()<redTime){
-            System.out.println(" PORAQUIPASA LA WEAAAA");
-            int dif2=tf.getSecond()-ti.getSecond();
-            turnRedLightON();
-            System.out.println(dif2 + " [s]   2");
-            tf=LocalTime.now();
-        }
+        this.greenTime=greenTime;
     }
 
     public void turnRedLightON() {
@@ -68,12 +38,7 @@ public class Semaforo3{
     public int getGreenTime() {
         return greenTime;
     }
-
-    public void setredTime(int redT) {
-        redTime=redT;
-    }
-
-    public static int getYellowLightTime() {
+    public static int getYellowTime() {
         return yellowTime;
     }
 
--- TestStage1.java
+++ TestStage1.java
@@ -1,17 +1,73 @@
 import java.io.*;
 import java.util.Scanner;
 import java.time.LocalTime;
+import java.time.Duration;
 public class TestStage1{
-      public static void main(String  args[]) throws IOException{
-          int N = Integer.parseInt(args[0]);
-          int redT = Integer.parseInt(args[1]);
-          LocalTime ti = LocalTime.now();
-          LocalTime tf = LocalTime.now(); 
-          while(tf.getSecond()-ti.getSecond()<N){
-              Semaforo3 sem1 = new Semaforo3(5);
-              sem1.setredTime(redT);
-              tf=LocalTime.now();
-          }
-          System.out.println("Fin Programa");
+    public static void main(String  args[]) throws IOException{
+        int N = Integer.parseInt(args[0]);
+        int redT = Integer.parseInt(args[1]);
+        LocalTime ti = LocalTime.now();
+        LocalTime tf = LocalTime.now();
+        Semaforo3 sem1 = new Semaforo3(5);
+        long telap = Duration.between(ti,tf).toMillis()/1000;
+        long tselap;
+        sem1.redTime=redT; 
+        while(telap<N){
+            System.out.println(telap);
+            LocalTime tis = LocalTime.now();
+            LocalTime tfs = LocalTime.now(); 
+            //verde
+            tselap = Duration.between(tis,tfs).toMillis()/1000;
+            if(telap>N)break;
+            while(tselap<sem1.getGreenTime()){
+                tfs=LocalTime.now();
+                tselap = Duration.between(tis,tfs).toMillis()/1000;
+                tf=LocalTime.now();
+                telap = Duration.between(ti,tf).toMillis()/1000;
+                if(telap>=N)break;
+                sem1.turnGreenLightON();
+                //System.out.println(tselap+" "+ sem1.getGreenTime()+" " + telap);
+                System.out.println( tselap + "[s] 0 Verde");
+
+            }
+            //amarillo
+            if(telap>=N)break;
+            tis = LocalTime.now();
+            tfs = LocalTime.now(); 
+            tselap = Duration.between(tis,tfs).toMillis()/1000;
+            while(tselap<sem1.getYellowTime()){
+                tfs=LocalTime.now();
+                tf=LocalTime.now();
+                tselap = Duration.between(tis,tfs).toMillis()/1000;
+                telap = Duration.between(ti,tf).toMillis()/1000;
+                //System.out.println(tselap+" "+ sem1.getYellowTime()+" "+ telap);
+                if(telap>=N)break;
+                sem1.turnYellowLightON();
+                System.out.println(tselap + "[s]  1 Amarillo");
+            }
+            //Rojo
+            if(telap>=N)break;
+            tfs = LocalTime.now(); 
+            tis = LocalTime.now();
+            tselap = Duration.between(tis,tfs).toMillis()/1000;
+            while(tselap<redT){
+                tfs=LocalTime.now();
+                tf=LocalTime.now();
+                tselap = Duration.between(tis,tfs).toMillis()/1000;
+                telap = Duration.between(ti,tf).toMillis()/1000;
+                if(telap>=N)break;
+                sem1.turnRedLightON();
+                System.out.println(tselap + "[s]   2 Rojo");
+                //System.out.println(tselap+" "+ redT+" "+ telap);
+            }
+            if(telap>=N)break;
+
+        }
+        System.out.println("Fin Programa tiempo total LOOP = "+telap);
     }
+    // public long getTimeElapsed(LocalTime t_inicial, LocalTime t_final){
+    //     tf=LocalTime.now();
+    //     long telapx = Duration.between(ti,tf).toMillis()/1000;
+    //     return telapx;
+    // }
 }
