aa9e249a0f3e9f811fcd0b272a9317020b3aa785
paula.amigo <paula.amigo@sansano.usm.cl>
2019-04-03 12:54:38 -0300

    Implementacion DetectorRequerimiento

--- Stage2/DetectorRequerimiento.java
+++ Stage2/DetectorRequerimiento.java
@@ -2,14 +2,23 @@ public class DetectorRequerimiento{
     boolean state;
     
     public boolean isON(){
-        return null;
+        return state;
     }
     public void setOn(){
+	state = true;
 
     }
     public void setOff(){
+	state = false;
 
     }
-    //public String toString(){}
+    public String toString(){
+    	if(state){
+		return ("1");
+	}
+	else{
+		return("0");
+	}
+    }
 
 }
