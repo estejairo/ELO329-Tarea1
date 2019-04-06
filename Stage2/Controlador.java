66ed790e730b717da37198df0482acdfea422191
LUIS IGNACIO BAHAMONDES PASTÉN <luis.bahamondes.14@sansano.usm.cl>
2019-04-05 22:14:23 -0300

    test commit

--- Stage2/Controlador.java
+++ Stage2/Controlador.java
@@ -1,9 +1,19 @@
+import java.lang.Object;
 public class Controlador{
     SemaforoP semp;
     DetectorRequerimiento boton;
     public Controlador(DetectorRequerimiento boton, SemaforoP semp){
     }
-    public manageTraffic(){
-        
+    public void run(){
+        try {
+            Thread.sleep(1000);   
+        }
+        catch (Exception e) {
+
+            //TODO: handle exception
+        }
+    }
+    public manageTraffic()throws Exception{
+//esto es un comentario
     }
 }
--- Stage2/TestStage2.java
+++ Stage2/TestStage2.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import java.time.LocalTime;
 import java.time.Duration;
 import java.util.Timer;
+import java.lang.Object;
+
 public class TestStage2{
     
 }
