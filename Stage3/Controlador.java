f1a6492b70347709d2aed9c18f33751b60be99e9
jairo.gonzalez.13 <jairo.gonzalez.13@Aragorn.elo.utfsm.cl>
2019-04-05 22:15:35 -0300

    Stage 3 first commit

---
+++ Stage3/Controlador.java
@@ -0,0 +1,9 @@
+public class Controlador{
+    SemaforoDeGiro semg;
+    DetectorRequerimiento boton;
+    public Controlador(DetectorRequerimiento sensorInductivo, SemaforoDeGiro semg){
+    }
+    public manageTraffic(){
+        
+    }
+}
---
+++ Stage3/TestStage3.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.Scanner;
+import java.time.LocalTime;
+import java.time.Duration;
+import java.util.Timer;
+public class TestStage3{
+    
+}
