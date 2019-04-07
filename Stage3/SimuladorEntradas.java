307e8942106edbfaf88348e5e556681c4fc79b0d
jairo.gonzalez.13 <jairo.gonzalez.13@Aragorn.elo.utfsm.cl>
2019-04-06 22:07:51 -0300

    Implementacion SimuladorEntradas

--- Stage3/SimuladorEntradas.java
+++ Stage3/SimuladorEntradas.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class SimuladorEntradas implements ActionListener{
     DetectorDeRequerimiento sensorInductivo;
     String filename;
+
+
+    File file = new File(filename);
+    Scanner entrada = new Scanner(file);
+    String linea;
+
     public SimuladorEntradas(DetectorDeRequerimiento sI, String flnm) {
         sensorInductivo = sI;
         filename = flnm;
     }
     public void actionPerformed(ActionEvent event) {
         sensorInductivo.setOn();
+        if (entrada.hasNextLine()) {
+            linea = entrada.nextLine();
+            if (linea=="1"){
+                sensorInductivo.setOn();
+            }
+        }
+        else {
+            entrada.close();
+            exit(0);
+        }
+        
     }
 }
