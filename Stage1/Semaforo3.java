9cab765fb8e4156efe053be67e774dfe381c8833
paula.amigo <paula.amigo@sansano.usm.cl>
2019-04-13 23:30:47 -0300

    cambio en el sistema de timing stage1, se agregan comentarios

--- Stage1/Semaforo3.java
+++ Stage1/Semaforo3.java
@@ -1,47 +1,64 @@
-import java.util.Timer;
-import java.io.*;
-import java.util.Scanner;
-//import java.time.LocalTime;
-//import java.time.Duration;
+////////////////////////////////////////////////////////////////
+//
+//  El siguiente codigo implementa un semaforo de 3 luces, 
+//  con tiempo de amarillo fijo, métodos para cambio de luces,
+//  para solicitar el tiempo de encendido y para imprimir el
+//  estado actual del semaforo.
+//
+////////////////////////////////////////////////////////////////
+
+
 public class Semaforo3{
     
-    boolean red,green,yellow;
+    // Estados del semaforo representados en luces
+    boolean red,green,yellow; 
 
+    // Luces con tiempo de encendido variable
     int redTime, greenTime;
     
+    // Luces con tiempo de encendido fijo
     static int yellowTime= 3; 
 
+    // Constructor del semaforo por defecto
     public Semaforo3(){}
 
+    // Constructor que incluye configuracion de luz verde
     public Semaforo3 (int greenTime) {
         this.greenTime=greenTime;
     }
 
+    // Metodo para encender la luz roja, apaga las otras
     public void turnRedLightON() {
         green = false;
         yellow = false;
         red = true;
     }
 
+    // Metodo para encender la luz amarilla, apaga las otras
     public void turnYellowLightON() {
         green = false;
         red = false;
         yellow = true;
     }
 
+    // Metodo para encender la luz verde, apaga las otras
     public void turnGreenLightON() {
         yellow = false;
         red = false;
         green = true;
     }
 
+    // Metodo que entrega el tiempo de duracion de la luz verde
     public int getGreenTime() {
         return greenTime;
     }
+
+    // Metodo que entrega el tiempo de duracion de la luz amarilla
     public static int getYellowTime() {
         return yellowTime;
     }
 
+    // Metodo que habilita la capacidad de imprimir el estado actual del semaforo
     public String toString() {
         if (red){
             return("2");
--- Stage1/TestStage1.java
+++ Stage1/TestStage1.java
@@ -1,74 +1,87 @@
-import java.io.*;
-import java.util.Scanner;
-import java.time.LocalTime;
-import java.time.Duration;
+//////////////////////////////////////////////////////////////////////////////////
+//                                                                              //
+//  El siguiente codigo permite probar el semaforo de 3 luces.                  //
+//  Para utilizarlo, se debe ejecutar entregando en primer lugar el tiempo      //
+//  en segundos que se operara el semaforo, y luego se debe indicar el          //
+//  el tiempo de encendido para la luz roja.                                    //
+//  Ejemplo: java TestStage1 30 5                                               //
+//  Donde 30 indica una operacion de 30 segundos, y el 5 indica una luz roja    //
+//  de 5 segundos de operacion.                                                 //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
 
+// Clase principal
 public class TestStage1{
-    public static void main(String  args[]) throws IOException{
-        int N = Integer.parseInt(args[0]);
-        int redT = Integer.parseInt(args[1]);
-        LocalTime ti = LocalTime.now();
-        LocalTime tf = LocalTime.now();
-        Semaforo3 sem1 = new Semaforo3(5);
-        long telap = Duration.between(ti,tf).toMillis()/1000;
-        long tselap;
-        sem1.redTime=redT; 
-        while(telap<N){
-            System.out.println(telap);
-            LocalTime tis = LocalTime.now();
-            LocalTime tfs = LocalTime.now(); 
-            //verde
-            tselap = Duration.between(tis,tfs).toMillis()/1000;
-            if(telap>N)break;
-            while(tselap<sem1.getGreenTime()){
-                tfs=LocalTime.now();
-                tselap = Duration.between(tis,tfs).toMillis()/1000;
-                tf=LocalTime.now();
-                telap = Duration.between(ti,tf).toMillis()/1000;
-                if(telap>=N)break;
-                sem1.turnGreenLightON();
-                //System.out.println(tselap+" "+ sem1.getGreenTime()+" " + telap);
-                System.out.println( tselap + "[s] 0 Verde");
 
-            }
-            //amarillo
-            if(telap>=N)break;
-            tis = LocalTime.now();
-            tfs = LocalTime.now(); 
-            tselap = Duration.between(tis,tfs).toMillis()/1000;
-            while(tselap<sem1.getYellowTime()){
-                tfs=LocalTime.now();
-                tf=LocalTime.now();
-                tselap = Duration.between(tis,tfs).toMillis()/1000;
-                telap = Duration.between(ti,tf).toMillis()/1000;
-                //System.out.println(tselap+" "+ sem1.getYellowTime()+" "+ telap);
-                if(telap>=N)break;
-                sem1.turnYellowLightON();
-                System.out.println(tselap + "[s]  1 Amarillo");
-            }
-            //Rojo
-            if(telap>=N)break;
-            tfs = LocalTime.now(); 
-            tis = LocalTime.now();
-            tselap = Duration.between(tis,tfs).toMillis()/1000;
-            while(tselap<redT){
-                tfs=LocalTime.now();
-                tf=LocalTime.now();
-                tselap = Duration.between(tis,tfs).toMillis()/1000;
-                telap = Duration.between(ti,tf).toMillis()/1000;
-                if(telap>=N)break;
-                sem1.turnRedLightON();
-                System.out.println(tselap + "[s]   2 Rojo");
-                //System.out.println(tselap+" "+ redT+" "+ telap);
-            }
-            if(telap>=N)break;
+    // Main
+    public static void main(String args[]){
+        int greenTime = 5; //Tiempo en verde, arbitrario
+        int N = Integer.parseInt(args[0]); //tiempo en segundos ingresado, trasnformado a entero
+        Semaforo3 sem = new Semaforo3(greenTime); //Construccion del semaforo
+        sem.redTime = Integer.parseInt(args[1]); //Seteo del tiempo de encendido para el rojo
+        
+        //Variables auxiliares para contar el tiempo transcurrido en cada luz
+        int currentRedTime = 1;
+        int currentYellowTime = 1;
+        int currentGreenTime = 1;
 
+        //Configuracion del estado inicial del semaforo
+        sem.turnRedLightON();
+
+        //Ciclo de N segundos de duracion (aprox)
+        for (int i=0; i<=N; i++){
+            // Logica para encender las luces
+            if ((currentRedTime < sem.redTime)&&(sem.red)){ //verifica si esta en rojo y aun queda tiempo
+                currentRedTime+=1;
+                
+            }
+            else if ((currentRedTime == sem.redTime)&&(sem.red)){ //Verifica si esta en rojo y debe cambiar de luz
+                sem.turnYellowLightON();
+                currentRedTime = 1;
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+            else if ((currentYellowTime < sem.yellowTime)&&(sem.yellow)){ //verifica si esta en amarillo y aun queda tiempo
+                currentYellowTime+=1;
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+            else if ((currentYellowTime == sem.yellowTime)&&(sem.yellow)){ //Verifica si esta en rojo y debe cambiar de luz
+                sem.turnGreenLightON();
+                currentYellowTime = 1;
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+            else if ((currentGreenTime < sem.greenTime)&&(sem.green)){ //verifica si esta en verde y aun queda tiempo
+                currentGreenTime+=1;
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+            else if ((currentGreenTime == sem.greenTime)&&(sem.green)){ //Verifica si esta en rojo y debe cambiar de luz
+                sem.turnRedLightON();
+                currentGreenTime = 1;
+            }
+
+            System.out.println(i + "\t" + sem); //Estado del semaforo cada segundo
+
+            //Esperar 1 segundo
+            try{
+                Thread.sleep(1000);
+            } catch(InterruptedException e){
+                System.out.println(e);
+            }
         }
-        System.out.println("Fin Programa tiempo total LOOP = "+telap);
     }
-    // public long getTimeElapsed(LocalTime t_inicial, LocalTime t_final){
-    //     tf=LocalTime.now();
-    //     long telapx = Duration.between(ti,tf).toMillis()/1000;
-    //     return telapx;
-    // }
 }
