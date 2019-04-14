77456a55def5bf21797e168bf76c58ddb6803dda
paula.amigo <paula.amigo@sansano.usm.cl>
2019-04-14 00:10:36 -0300

    Comentarios extra stage 3

--- Stage3/SimuladorEntradas.java
+++ Stage3/SimuladorEntradas.java
@@ -1,3 +1,13 @@
+/////////////////////////////////////////////////////////////////////////////////////
+//
+//  El siguiente codigo implementa el comportamiento de un ActionListener,
+//  leyendo una nueva linea del archivo que simula las entradas cada vez que
+//  se gatilla un evento.
+//  El objeto recibe un detector de requerimiento y el texto procesado por Scanner.
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+
 import java.util.Scanner;
 import java.io.*;
 import java.awt.event.*;
@@ -5,26 +15,26 @@ import javax.swing.*;
 
 public class SimuladorEntradas implements ActionListener {
     DetectorRequerimiento sensorInductivo;
-    Scanner filename;
-    //Scanner entrada;
-    String linea;
+    Scanner filename; //Nombre del archivo procesado por Scanner
+    String linea;   //Linea del archivo
 
+    //Constructor. Recibe un detector y el objeto tipo Scanner con el texto
     public SimuladorEntradas(DetectorRequerimiento sI, Scanner flnm) {
         sensorInductivo = sI;
         filename = flnm;
     }
 
-
+    //Metodo que implementa la accion a realizar cuando se gatilla un evento
     public void actionPerformed(ActionEvent event) {
-        if (filename.hasNextLine()) {
-            linea = filename.nextLine();
-            if ((linea.compareTo("1"))==0){                
-                sensorInductivo.setOn();
-            }
+        if (filename.hasNextLine()) {   //Su hay una linea disponible para leer,
+            linea = filename.nextLine();//Se le asigna esa nueva linea a la variable.
+            if ((linea.compareTo("1"))==0){   //Si es un 1, se enciende el detector.             
+                sensorInductivo.setOn();        
+            }   //Si no hay "1" en la linea, no se hace nada
         }
-        else {
-            filename.close();
-            System.exit(0);
+        else {  //Si llega al final del archivo
+            filename.close();   //Se cierrra el Scanner
+            System.exit(0);     //Se sale del programa
         }
         
     }
--- Stage3/TestStage3.java
+++ Stage3/TestStage3.java
@@ -1,3 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+//  El siguiente codigo permite probar el semaforo de giro. Al ejecutarlo se
+//  debe indicar la ruta del archivo que contiene las señales de prueba para
+//  activar el sensor inductivo. Ej: java TestStage3 entradas.txt
+//  Las entradas son procesadas mediante Scanner, y procesadas en paralelo al
+//  metodo manageTraffic cada un segun con un ActionListener. El metodo
+//  manageTraffic es del controlador y se encarga de operar el semaforo segun
+//  los tiempos y las señales del sensor inductivo.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -5,21 +18,24 @@ import java.util.Scanner;
 
 public class TestStage3{
     public static void main(String  args[]) throws FileNotFoundException{
-        String filename = args[0];
-        int greenTime = 30;
-        int blinkingTime = 2;
+        String filename = args[0]; //nombre del archivo a leer (incluyendo formato, idealmente .txt)
+        int greenTime = 30; //Tiempo que dura la luz verde
+        int blinkingTime = 2;   //Tiempo que se mantiene parpadeando antes de apagarse
         
 
-        File file = new File(filename);
-        try(Scanner entrada = new Scanner(file);){
+        File file = new File(filename); //Se genera un archivo File que contenga al .txt
+        try(Scanner entrada = new Scanner(file);){ //Se procesa el archivo mediante Scanner
+            //Si se encuentra el archivo, se crean los objetos de semaforo, sensor, y controlador.
+            //Ademas se genera un SimuladorEntradas para leer el archivo.
             SemaforoDeGiro semaforo = new SemaforoDeGiro(greenTime, blinkingTime);
             DetectorRequerimiento sensorInductivo = new DetectorRequerimiento();
             ActionListener entradas = new SimuladorEntradas(sensorInductivo, entrada);
             Controlador ctrl = new Controlador(sensorInductivo, semaforo);
-       
+            
+            //El archivo se lee cada 1000 milisegundos segun lo definido en los metodos de SimuladorEntradas
             Timer t = new Timer(1000, entradas);
-            t.start();
-            ctrl.manageTraffic();
+            t.start(); //Comienza la lectura en paralelo
+            ctrl.manageTraffic(); //Se opera el semaforo, atento a las señales del sensor
         } catch (FileNotFoundException e) {
             System.out.println(e);
             System.exit(0);
